e6e64aab759e8be7cff977ad752b6df9d5caa233
iljaasd <iljaasd@gmail.com>
2022-03-28 11:14:43 +0200

    REFACTOR: changed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Assignment_7_Pokemon_Trainer';
+  title = 'Pokemon Trainer';
 }
