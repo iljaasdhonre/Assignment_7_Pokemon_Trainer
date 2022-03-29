3f71c432f736b06bb3dca96f96bef2667c8688cf
iljaasd <iljaasd@gmail.com>
2022-03-29 21:18:17 +0200

    login form renamed

--- src/app/components/login/login-form.component.ts
+++ src/app/components/login/login-form.component.ts
@@ -1,12 +1,12 @@
-import { LoginService } from './../../services/login.service';
+import { LoginService } from '../../services/login.service';
 import { Component} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Trainer } from 'src/app/models/trainer';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
 })
 export class LoginComponent {
 
@@ -19,10 +19,10 @@ export class LoginComponent {
     this.loginService.login(username)
       .subscribe({
         next: (trainer: Trainer) => {
-
+          console.log(trainer);
         },
         error: () =>{
-
+          console.log("something went wrong");
         }
       })
   }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login-page',
+  selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.css']
 })
