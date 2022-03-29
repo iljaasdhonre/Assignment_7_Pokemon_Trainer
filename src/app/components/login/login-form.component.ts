4be94df3cd387bab4219daa200888d9a1e015593
iljaasd <iljaasd@gmail.com>
2022-03-29 21:29:01 +0200

    navigate to profile

--- src/app/components/login/login-form.component.ts
+++ src/app/components/login/login-form.component.ts
@@ -2,6 +2,7 @@ import { LoginService } from '../../services/login.service';
 import { Component} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Trainer } from 'src/app/models/trainer';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -10,7 +11,9 @@ import { Trainer } from 'src/app/models/trainer';
 })
 export class LoginComponent {
 
-  constructor(private readonly loginService: LoginService) { }
+  constructor(
+    private readonly router: Router,
+    private readonly loginService: LoginService) { }
 
   public loginSubmit(loginForm: NgForm): void{
 
@@ -19,7 +22,7 @@ export class LoginComponent {
     this.loginService.login(username)
       .subscribe({
         next: (trainer: Trainer) => {
-          console.log(trainer);
+          this.router.navigateByUrl("/profile");
         },
         error: () =>{
           console.log("something went wrong");
